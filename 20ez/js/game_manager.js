b797328819ffafc4bb5f41824d0f334e5b2ab0a1
redd <107080537+redd-rl@users.noreply.github.com>
2022-11-14 13:17:37 +0100

    Update game_manager.js

--- 20ez/js/game_manager.js
+++ 20ez/js/game_manager.js
@@ -75,7 +75,7 @@ GameManager.prototype.addRandomTile = function () {
           !!self.grid.cells[cell.x + x][cell.y + y]
         ) {
           var tocheck = self.grid.cells[cell.x + x][cell.y + y];
-          if (Math.random() < 0.8 && tocheck.value < bvalue) {
+          if (true && tocheck.value < bvalue) {
             bcell = cell;
             bvalue = tocheck.value;
           }
